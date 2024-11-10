20b7e35a9308cf841b9882b6b043000068728280
Zack Ames <zames6584@gmail.com>
2024-11-10 08:58:26 +0700

    new policies

--- client/src/dojo/controller.ts
+++ client/src/dojo/controller.ts
@@ -1,16 +1,32 @@
 
 import Controller from "@cartridge/controller";
-import { account, username, } from "../stores";
+import { account, username, dojoStore} from "../stores";
 import manifest from "../../../contracts/manifest_dev.json";
+import { get } from "svelte/store";
 
 let contract_address = manifest.contracts[0].address;
+let planeteloContract = get(dojoStore).planetelo.address
+
 //let contract_address = "0x049d36570d4e46f48e99674bd3fcc8463d4990949b4c6bb434ee877b1830a794"
 export const controller = new Controller({
     policies: [
         {
             target: contract_address,
             method: "guess",
+        },
+        {
+            target: planeteloContract,
+            method: "queue",
+        },
+        {
+            target: planeteloContract,
+            method: "matchmake",
+        },
+        {
+            target: planeteloContract,
+            method: "settle",
         }
+
         // ... other policies
     ],
      rpc: "https://api.cartridge.gg/x/starknet/sepolia" // sepolia, mainnet, or slot. (default sepolia)
